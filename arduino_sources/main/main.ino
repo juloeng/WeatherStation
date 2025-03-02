9ec50e61bf594cb11745eda5816bd08bb5f1f1a7
juloeng <julesgallien163@gmail.com>
2025-03-02 15:40:09 +0100

    init: initial commit for arduino main program file

---
+++ arduino_sources/main/main.ino
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////
+// Author      : juloeng                                                      //
+// Date        : 02/03/2025                                                   //
+// Description : Main file for weather station with ESP32                     //
+////////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
